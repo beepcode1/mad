17fd786cf21504df3f4a3dd2439f469aa9a40ce5
beepcode1 <arnavcoc.av@gmail.com>
2025-01-01 19:07:12 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,15 @@
+package com.example.dreamjobcard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Optional: You can add functionality here, like setting dynamic data.
+    }
+}
